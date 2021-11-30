761e08fdcedd5bbb9e751e3cf9a4f15feb225c2b
jordynuyts123 <43341356+jordynuyts123@users.noreply.github.com>
2021-11-30 08:41:23 +0100

    Update several fixes

--- doodelaars/Assets/destroy.cs
+++ doodelaars/Assets/destroy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class destroy : MonoBehaviour
 {
@@ -8,30 +9,24 @@ public class destroy : MonoBehaviour
     public GameObject platformprefab;
     private GameObject myPlat;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.gameObject.name.StartsWith("Platform"))
         {
             Destroy(collision.gameObject);
             myPlat = (GameObject)Instantiate(platformprefab, new Vector2(Random.Range(-2.3f, 2.3f), doodler.transform.position.y + (5 + Random.Range(0.1f, 1.8f))), Quaternion.identity);
-
         }
         else if (collision.gameObject.name.StartsWith("Doodler"))
         {
+
             Handheld.Vibrate();
 
             AudioManager.PlaySound("falldown");
             Destroy(collision.gameObject);
         }
+
     }
+
 }
