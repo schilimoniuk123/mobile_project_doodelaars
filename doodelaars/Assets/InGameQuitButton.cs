825ba32b6c819a6cf470e42ddbda2cc25713476e
schilimoniuk123 <samuel.chilimoniuk@student.ucll.be>
2021-11-30 19:28:52 +0100

    update hide resume button - not done

--- doodelaars/Assets/InGameQuitButton.cs
+++ doodelaars/Assets/InGameQuitButton.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class InGameQuitButton : MonoBehaviour
 {
+
+    public GameObject resumeButton;
+    void Start()
+    {
+        
+        resumeButton.SetActive(false);
+    }
     public void QuitButton()
     {
         SceneManager.LoadScene("Start Menu");
+    }
+
+    public void PauseGame() 
+    {
+        ///TODO poepke genaaid - deze rommel fixe
+        resumeButton.SetActive(true);
+        Debug.Log("eneenjenjejnee");
+        Time.timeScale = 0;
+        Debug.Log("eneenjenjejnee");    
+    }
+
+    public void ResumeGame()
+    {
+        resumeButton.SetActive(false);
+        Time.timeScale = 1;
     }
 
 }
